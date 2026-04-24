708cff03abeee59f08cf97198e5e2dc243057caa
Igor Kolodziej <igor.kolodziej@proton.me>
2026-04-24 22:41:39 +0200

    Handle missing customer during enrichment

--- src/main/scala/com/team/pipeline/application/enrichment/EventEnricher.scala
+++ src/main/scala/com/team/pipeline/application/enrichment/EventEnricher.scala
@@ -2,6 +2,7 @@ package com.team.pipeline.application.enrichment
 
 import com.team.pipeline.application.validation.EmailHasher
 import com.team.pipeline.domain.CustomerProfile
+import com.team.pipeline.domain.CustomerNotFound
 import com.team.pipeline.domain.EnrichedPaymentEvent
 import com.team.pipeline.domain.NormalizedPaymentEvent
 
@@ -16,3 +17,12 @@ object EventEnricher:
       customer = customer,
       hashedCustomerEmail = EmailHasher.sha256(emailSalt).hash(customer.email)
     )
+
+  def enrichOption(
+      event: NormalizedPaymentEvent,
+      customer: Option[CustomerProfile],
+      emailSalt: String
+  ): Either[CustomerNotFound, EnrichedPaymentEvent] =
+    customer match
+      case Some(profile) => Right(enrich(event, profile, emailSalt))
+      case None          => Left(CustomerNotFound(event.customerId))
