407a2fee8b6c3ae776a35ede38ff45b0b5c390a0
Igor Kolodziej <igor.kolodziej@proton.me>
2026-04-24 17:43:45 +0200

    Wire startup through typed config

--- src/main/scala/com/team/pipeline/Main.scala
+++ src/main/scala/com/team/pipeline/Main.scala
@@ -1,7 +1,12 @@
 package com.team.pipeline
 
 import cats.effect.{IO, IOApp}
+import com.team.pipeline.config.AppConfig
 
 object Main extends IOApp.Simple:
   def run: IO[Unit] =
-    IO.println("Payment Event Processing Pipeline started.")
+    AppConfig.load.flatMap { config =>
+      IO.println(
+        s"Payment Event Processing Pipeline started. input=${config.app.inputFile}, output=${config.app.outputDir}"
+      )
+    }
