6d54cd96e98f4c6239e3c6074c77830f61e399f1
Igor Kolodziej <igor.kolodziej@proton.me>
2026-04-24 17:44:06 +0200

    Create output directory on startup

--- src/main/scala/com/team/pipeline/Main.scala
+++ src/main/scala/com/team/pipeline/Main.scala
@@ -3,10 +3,14 @@ package com.team.pipeline
 import cats.effect.{IO, IOApp}
 import com.team.pipeline.config.AppConfig
 
+import java.nio.file.Files
+
 object Main extends IOApp.Simple:
   def run: IO[Unit] =
-    AppConfig.load.flatMap { config =>
-      IO.println(
+    for
+      config <- AppConfig.load
+      _ <- IO.blocking(Files.createDirectories(config.app.outputDir))
+      _ <- IO.println(
         s"Payment Event Processing Pipeline started. input=${config.app.inputFile}, output=${config.app.outputDir}"
       )
-    }
+    yield ()
