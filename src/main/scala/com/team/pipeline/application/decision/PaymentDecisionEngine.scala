089e502a157faca8f431f9a8f9c64c6921b8d575
Igor Kolodziej <igor.kolodziej@proton.me>
2026-04-26 12:00:35 +0200

    Add not-evaluated risk decision

--- src/main/scala/com/team/pipeline/application/decision/PaymentDecisionEngine.scala
+++ src/main/scala/com/team/pipeline/application/decision/PaymentDecisionEngine.scala
@@ -36,6 +36,7 @@ object PaymentDecisionEngine:
 
   private def finalDecisionFor(risk: RiskAssessment): FinalDecision =
     risk.decision match
-      case RiskDecision.Approve => FinalDecision.Accepted
-      case RiskDecision.Review  => FinalDecision.Review
-      case RiskDecision.Block   => FinalDecision.BlockedByRisk
+      case RiskDecision.Approve      => FinalDecision.Accepted
+      case RiskDecision.Review       => FinalDecision.Review
+      case RiskDecision.Block        => FinalDecision.BlockedByRisk
+      case RiskDecision.NotEvaluated => FinalDecision.Declined
